1bd066c39a15804577152995855e7e211f31c3bd
DrBones <rogue.inbox@yahoo.de>
2011-01-14 17:03:44 +0100

    written basic functions

--- main.cpp
+++ main.cpp
@@ -5,9 +5,25 @@ int doubleNumber(int x)
   return x*2;
 }
 
+int ReadNumber()
+{
+  using namespace std;
+  cout << "Please type in number: " << endl;
+  int x;
+  cin >> x;
+  return  x;
+}
+
+void WriteAnswer(int x)
+{
+  using namespace std;
+  cout << "The Answer is:"<< x << endl;
+}
 int main()
 {
-   using namespace std;
-   cout << add(2, 3) << endl;
+  int x = ReadNumber();
+  int y = ReadNumber();
+
+  WriteAnswer(x + y);
    return 0;
 }
